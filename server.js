569baa2754caaaef9a44fceadd1adf21bb5c76c4
JericoCancel1 <jerico.cancel@upr.edu>
2024-11-12 11:14:32 -0800

    Added home page

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express =require("express");
+const path = require("path");
 const app =express();
 
 
 const port= process.env.PORT || 4000;
 
 
-app.get("/", function(req,res){
-    res.json({
-        message:"Success",
-        data: "Api Running"
-    })
-})
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 
 app.listen(port, function(){
